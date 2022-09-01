1f05aef309804a546e816d32f0bb1b583d385174
Charly Brown <2131406-carlosmonti@users.noreply.gitlab.com>
2022-09-01 17:51:17 -0300

    feat: expresss vs sqlite3 working

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+
+// sqlite
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('/data/customer.db')
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/hit', (req, res) => {
+  db.serialize(() => {
+    db.run('CREATE TABLE IF NOT EXISTS hits (hit TEXT)')
+    const stmt = db.run('INSERT INTO hits VALUES (?)', ['hit'])
+
+    db.get('SELECT COUNT(*) FROM hits', (err, row) => {
+      console.log('count row: ', row, err)
+      res.send(row)
+    })
+  })
 })
 
 app.listen(port, () => {
